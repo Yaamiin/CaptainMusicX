6d206a1b1f4a656177fa6d9e8bd517577ccab529
HEXOROP <85221480+HEXOROP@users.noreply.github.com>
2021-10-15 13:58:00 +0530

    Add files via upload

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -1,12 +1,13 @@
 from os import path
 
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 
 from config import BOT_NAME as bn, DURATION_LIMIT
 from helpers.errors import DurationLimitError
 
 ydl_opts = {
     "format": "bestaudio/best",
+    "verbose": True,
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
